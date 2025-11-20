52e9e1c2d8cea5a83b14b05e9251d426bbb7c4d8
Atif <atif58136@gmail.com>
2025-11-20 19:51:46 +0500

    Made colors be configurable form a file

--- src/main/java/net/atif/buildnotes/client/BuildnotesClient.java
+++ src/main/java/net/atif/buildnotes/client/BuildnotesClient.java
@@ -1,7 +1,9 @@
 package net.atif.buildnotes.client;
 
 import io.netty.buffer.Unpooled;
+import net.atif.buildnotes.data.ColorConfig;
 import net.atif.buildnotes.data.PermissionLevel;
+import net.atif.buildnotes.gui.helper.Colors;
 import net.atif.buildnotes.gui.screen.MainScreen;
 import net.atif.buildnotes.data.TabType;
 import net.atif.buildnotes.network.ClientPacketHandler;
@@ -22,11 +24,13 @@ import net.fabricmc.fabric.api.networking.v1.PacketSender;
 public class BuildnotesClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        ColorConfig.loadColors();
         KeyBinds.register();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (KeyBinds.openGuiKey.wasPressed()) {
                 if (client.currentScreen == null) {
+                    Colors.reload();
                     client.setScreen(new MainScreen(TabType.NOTES));
                 }
             }
